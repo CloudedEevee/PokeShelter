fb57a3c8e12efca4f05779c4c331aa8f2ea1cd7c
CloudedEevee <makayla.s.schatz@gmail.com>
2024-01-24 21:50:22 -0700

    Began Client-Side Setup
    Added JSX files for Result Cards, Created Temp Icon, Cleaned up default App.jsx, imported react-router-dom for routing.

---
+++ PokeShelter/src/components/resultcards/Quicklook.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Quicklook = (props) => {
+
+
+    //////////////////////////////////////////////////////////// Return
+    return (
+        <div>
+            <p style={{ margin: ".2rem" }}>Pokename</p>
+            <p style={{ margin: ".2rem" }}>Set Nickname</p>
+            <button style={{ padding: ".3rem .5rem", textAlign: "center", backgroundColor:"brown", boxShadow: "4px 4px black"}}>Add to Party</button>
+        </div>
+    )
+}
+
+
+export default Quicklook;
