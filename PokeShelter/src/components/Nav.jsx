c2410fe1f4a1af8d90b3550bec765f8e4573b64a
Joe Tomlin <joetomlin8@gmail.com>
2024-01-25 16:28:04 -0800

    added Client styling to Nav and Detail Page

---
+++ PokeShelter/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+
+    return (
+        <nav style={{display: "flex", flexDirection: "column", alignItems: "center", margin: "1rem"}}>
+            <h1 style={{color: "yellow", textShadowColor: "blue", textShadowRadius: 3,}}>PokeShelter</h1>
+            <h5>Ever wonder what happened to those Daycare eggs?...</h5>
+        </nav>
+    )
+}
+
+export default Nav
---
+++ PokeShelter/src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Search = () => {
+
+    return (
+        <div>
+            <input type="text" placeholder="Find your partner. . ."/>
+        </div>
+    )
+}
+
+export default Search
