2c1e1bb33faa85093568b463ba5f7ff776f309dd
CloudedEevee <makayla.s.schatz@gmail.com>
2024-01-27 23:59:23 -0700

    ResCard styling, Nickname input began, enFlavor added

--- PokeShelter/client/src/components/Nav.jsx
+++ PokeShelter/client/src/components/Nav.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Nav = () => {
 
     return (
-        <nav style={{display: "flex", flexDirection: "column", alignItems: "center", margin: "1rem"}}>
+        <nav style={{display: "flex", flexDirection: "column", alignItems: "center", margin: ".5rem"}}>
             <h1 style={{color: "yellow", textShadowColor: "blue", textShadowRadius: 3,}}>PokeShelter</h1>
             <h5>Ever wonder what happened to those Daycare eggs?...</h5>
         </nav>
     )
 }
 
-export default Nav
+export default Nav;
