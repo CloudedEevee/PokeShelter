8806da8ff48f822b87033002320ad3e6081f9128
Trey Vickery <treyvickery@Treys-MacBook-Pro.local>
2024-01-30 12:29:08 -0600

    Fixed add to party

--- PokeShelter/client/src/components/resultcards/ResultCard.jsx
+++ PokeShelter/client/src/components/resultcards/ResultCard.jsx
@@ -1,67 +1,71 @@
-import React, { useEffect, useState } from "react"; 
-import './resultcards.css'
+import React, { useEffect, useState } from "react";
+import "./resultcards.css";
 import Quicklook from "./quicklook";
 import PokeballIcon from "../general/PokeballIcon";
 import Btn from "../general/Btn";
 import axios from "axios";
 
 const ResultCard = (props) => {
-    const {pokemonCard, setPokemonCard, btnAction, setBtnAction, pokeParty, setPokeParty} = props;
-    const [nickname, setNickname] = useState("");
+  const {
+    pokemonCard,
+    setPokemonCard,
+    btnAction,
+    setBtnAction,
+    pokeParty,
+    setPokeParty,
+  } = props;
+  const [nickname, setNickname] = useState("");
 
-    useEffect(() => {
-        let action = "Add to Party";
-        setBtnAction(action);
-    },[])
-    //////////////////////////////////////////////////////////// Add pokemon to DB >>NOT WORKING<<
-    const addToParty = (e) => {
-        addNickname();
-        await saveToDB(pokemonCard);
-    }
-    
-    const addNickname = () => {
-        return new Promise((res, rej) => {
-            setPokemonCard((prevPokeCard) =>({...prevPokeCard, 'nickname': nickname}));\
-        })
-    }
+  useEffect(() => {
+    let action = "Add to Party";
+    setBtnAction(action);
+  }, []);
 
-    const saveToDB = (data) => {
-        console.log(data);
-        axios 
-            .post(`http://localhost:8000/api/pokemon`, data)
-            .then(res => {
-                console.log(res.data);
-                setPokeParty( (prevPokeParty) => [...prevPokeParty, res.data]);
-            })
-            .catch(err => console.log(err))
-    }
+  const addToParty = async (e) => {
+    await addNickname();
+    await saveToDB({ nickname }); // Only send the necessary data to the server
+  };
 
-    //////////////////////////////////////////////////////////// Return
-    return (
-        <>
-            {
-                pokemonCard.dex ? 
-                <div className="resultCard">
-                    <div className="resTop">
-                        <PokeballIcon />
-                        <Quicklook 
-                            pokemonCard={pokemonCard}
-                            nickname={nickname}
-                            setNickname={setNickname}
-                            />
-                    </div>
-                    <Btn 
-                        btnAction={btnAction}
-                        onSubProp={addToParty} />
-                    
-                </div>
-                :
-                null
-            }
-        </>
-    )
+  const addNickname = () => {
+    return new Promise((res) => {
+      setPokemonCard((prevPokeCard) => ({
+        ...prevPokeCard,
+        nickname: nickname,
+      }));
+      res();
+    });
+  };
 
-}
+  const saveToDB = (data) => {
+    console.log("Data being sent to the server:", data);
 
+    axios
+      .post(`http://localhost:8000/api/pokemon`, data)
+      .then((res) => {
+        console.log("Server response:", res.data);
+        setPokeParty((prevPokeParty) => [...prevPokeParty, res.data]);
+      })
+      .catch((err) => console.error("Error in POST request:", err));
+  };
+
+  return (
+    <>
+      {pokemonCard.dex ? (
+        <div className="resultCard">
+          <div className="resTop">
+            <PokeballIcon />
+            <Quicklook
+              pokemonCard={pokemonCard}
+              nickname={nickname}
+              setNickname={setNickname}
+            />
+          </div>
+          <Btn btnAction={btnAction} onSubProp={addToParty} />
+        </div>
+      ) : null}
+    </>
+  );
+};
 
 export default ResultCard;
+
