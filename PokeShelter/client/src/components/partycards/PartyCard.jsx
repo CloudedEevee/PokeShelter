db89a33ebc925a1417ed123849b5d25e22c7d79b
Trey Vickery <treyvickery@Treys-MacBook-Pro.local>
2024-01-31 23:50:25 -0600

    Edit button

--- PokeShelter/client/src/components/partycards/PartyCard.jsx
+++ PokeShelter/client/src/components/partycards/PartyCard.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const PartyCard = (props) => {
   const { pokeParty, setPokeParty } = props;
+
+  const [editMode, setEditMode] = useState({});
+  const [newNickname, setNewNickname] = useState({});
 
   const releasePoke = (idFromMap) => {
     axios
@@ -13,6 +16,29 @@ const PartyCard = (props) => {
           (allOtherPokemon) => allOtherPokemon._id !== idFromMap
         );
         setPokeParty(newPokeParty);
+      })
+      .catch((err) => {
+        console.log("Something went wrong: ", err);
+      });
+  };
+
+  const handleEdit = (id, currentNickname) => {
+    setEditMode((prev) => ({ ...prev, [id]: true }));
+    setNewNickname((prev) => ({ ...prev, [id]: currentNickname }));
+  };
+
+  const handleEditSubmit = (id) => {
+    setEditMode((prev) => ({ ...prev, [id]: false }));
+
+    // Make the API call to update the nickname
+    axios
+      .put(`http://localhost:8000/api/pokemon/${id}`, { nickname: newNickname[id] })
+      .then((res) => {
+        console.log("Updated Pokemon:", res);
+        // Assuming the API returns the updated Pokemon, update the state
+        setPokeParty((prev) =>
+          prev.map((p) => (p._id === id ? { ...p, nickname: newNickname[id] } : p))
+        );
       })
       .catch((err) => {
         console.log("Something went wrong: ", err);
@@ -30,14 +56,31 @@ const PartyCard = (props) => {
               <button className="red-btn" onClick={() => releasePoke(onePokemon._id)}>
                 Release
               </button>
+              <button className="edit-btn" onClick={() => handleEdit(onePokemon._id, onePokemon.nickname)}>
+                Edit
+              </button>
+
+              {editMode[onePokemon._id] && (
+                <div>
+                  <input
+                    type="text"
+                    value={newNickname[onePokemon._id] || ""}
+                    onChange={(e) =>
+                      setNewNickname((prev) => ({ ...prev, [onePokemon._id]: e.target.value }))
+                    }
+                  />
+                  <button onClick={() => handleEditSubmit(onePokemon._id)}>Save</button>
+                  
+                </div>
+              )}
             </>
           )}
         </div>
-        
       ))}
     </>
   );
 };
 
 export default PartyCard;
+
 
