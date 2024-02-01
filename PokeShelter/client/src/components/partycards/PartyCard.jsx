aacd812d1c4d1ae74b8b3f86548b63f726a57f4a
Trey Vickery <treyvickery@Treys-MacBook-Pro.local>
2024-01-31 23:33:43 -0600

    Your commit message here

--- PokeShelter/client/src/components/partycards/PartyCard.jsx
+++ PokeShelter/client/src/components/partycards/PartyCard.jsx
@@ -1,37 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import axios from "axios";
 
 const PartyCard = (props) => {
-    const {
-        pokeParty,
-        setPokeParty,
-    } = props;
+  const { pokeParty, setPokeParty } = props;
 
-    const releasePoke = (idFromMap) => {
-        axios.delete(`http://localhost:8000/api/pokemon/${idFromMap}`)
-        .then((res) => {
-            console.log("Released Pokemon:", res)
-            const newPokeParty = pokeParty.filter((allOtherPokemon) => allOtherPokemon._id !== idFromMap)
-            setPokeParty(newPokeParty)
-        })
-        .catch((err) => {
-            console.log("Something went wrong: ", err)
-        })
-    }
-
-    return (
-        <>
-            { 
-                pokeParty.map((onePokemon, i) => (
-                    <div className="partyCard" key="i">
-                        <h3>{onePokemon.name.charAt(0).toUpperCase() + onePokemon.name.slice(1)}</h3>
-                        <h4>{onePokemon.nickname}</h4>
-                        <button className='red-btn' onClick={() => releasePoke(onePokemon._id)}>Release</button>
-                    </div>
-                ))
-            }
-        </>
+  const releasePoke = (idFromMap) => {
+    axios
+      .delete(`http://localhost:8000/api/pokemon/${idFromMap}`)
+      .then((res) => {
+        console.log("Released Pokemon:", res);
+        const newPokeParty = pokeParty.filter(
+          (allOtherPokemon) => allOtherPokemon._id !== idFromMap
         );
-    };
-    
-    export default PartyCard;
+        setPokeParty(newPokeParty);
+      })
+      .catch((err) => {
+        console.log("Something went wrong: ", err);
+      });
+  };
+
+  return (
+    <>
+      {pokeParty.map((onePokemon, i) => (
+        <div className="partyCard" key={i}>
+          {onePokemon && onePokemon.name && (
+            <>
+              <h3>{onePokemon.name.charAt(0).toUpperCase() + onePokemon.name.slice(1)}</h3>
+              <h4>{onePokemon.nickname}</h4>
+              <button className="red-btn" onClick={() => releasePoke(onePokemon._id)}>
+                Release
+              </button>
+            </>
+          )}
+        </div>
+        
+      ))}
+    </>
+  );
+};
+
+export default PartyCard;
+
